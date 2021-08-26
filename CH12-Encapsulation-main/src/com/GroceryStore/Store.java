618ed236e584150357979d56bc7b33ac5e497071
MGreco2112 <mgreco2112@tutanota.com>
2021-08-26 18:13:38 -0400

    finished addToInventory methods

--- CH12-Encapsulation-main/src/com/GroceryStore/Store.java
+++ CH12-Encapsulation-main/src/com/GroceryStore/Store.java
@@ -5,6 +5,7 @@ import com.GroceryStore.Products.Fruit;
 import com.GroceryStore.Products.Product;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
@@ -25,7 +26,7 @@ public class Store {
     }
 
     // TODO: Drink version
-    public void addToInventory() {
+    public void addDrinkToInventory() {
         System.out.println("What is the name of the Drink?");
 
         String name = scanner.nextLine();
@@ -42,15 +43,53 @@ public class Store {
 
         String description =  scanner.nextLine();
 
+        System.out.println("What is the total volume of this Drink?");
 
+        String volume = scanner.nextLine();
 
-        Drink drink = new Drink();
+        System.out.println("How will this drink be measured?\n" + Arrays.toString(Drink.UNITS));
+
+        String units = scanner.nextLine();
+
+        Drink drink = new Drink(name, Integer.parseInt(price), id, description, Integer.parseInt(volume), units);
         addToInventory(drink);
     };
 
     // TODO: Friut version
-    public void addToInventory() {
-        Fruit fruit = new Fruit();
+    public void addFruitToInventory() {
+
+        System.out.println("What is the name of the Fruit?");
+
+        String name = scanner.nextLine();
+
+        System.out.println("How much will the Fruit cost (in cents):");
+
+        String price = scanner.nextLine();
+
+        System.out.println("Enter the id number of the Fruit:");
+
+        String id = scanner.nextLine();
+
+        System.out.println("Enter a product description:");
+
+        String description =  scanner.nextLine();
+
+        System.out.println("What is the hardness of this Fruit?");
+
+        String hardness = scanner.nextLine();
+
+        System.out.println("Is this Fruit organic?\n(y)es\n(n)o");
+
+        String organic = scanner.nextLine();
+
+        boolean isOrganic = false;
+
+        switch (organic) {
+            case "y" :
+                isOrganic = true;
+        }
+
+        Fruit fruit = new Fruit(name, Integer.parseInt(price), id, description, Integer.parseInt(hardness), isOrganic);
         addToInventory(fruit);
     };
 

1cc50df1b41e41b262365991e88093ad6de79822
MGreco2112 <mgreco2112@tutanota.com>
2021-08-26 17:55:23 -0400

    working on the todo methods

--- CH12-Encapsulation-main/src/com/GroceryStore/Store.java
+++ CH12-Encapsulation-main/src/com/GroceryStore/Store.java
@@ -6,11 +6,13 @@ import com.GroceryStore.Products.Product;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Store {
     private List<Product> inventory = new ArrayList<>();
     private int balance = 500_00;
     private String name;
+    private Scanner scanner = new Scanner(System.in);
 
     public Store(String name) {
         this.name = name;
@@ -18,12 +20,30 @@ public class Store {
 
     // TODO: add the given product to the collection.
     public void addToInventory(Product product) {
-
+        inventory.add(product);
 
     }
 
     // TODO: Drink version
     public void addToInventory() {
+        System.out.println("What is the name of the Drink?");
+
+        String name = scanner.nextLine();
+
+        System.out.println("How much will the Drink cost (in cents):");
+
+        String price = scanner.nextLine();
+
+        System.out.println("Enter the id number of the Drink:");
+
+        String id = scanner.nextLine();
+
+        System.out.println("Enter a product description:");
+
+        String description =  scanner.nextLine();
+
+
+
         Drink drink = new Drink();
         addToInventory(drink);
     };
